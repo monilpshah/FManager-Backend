c0083b03a83a9f0849ef9c26f8b66f8afddfeca0
monilpshah <monil31199@gmail.com>
2019-10-05 11:31:12 +0530

    FManager

--- dbconnection.js
+++ dbconnection.js
@@ -1,10 +1,15 @@
 var mysql=require('mysql');
  var connection=mysql.createPool({
  
-host:'localhost',
- user:'root',
- password:'',
- database:'FManager'
+// host:'localhost',
+//  user:'root',
+//  password:'',
+//  database:'FManager'
  
+ 
+host:'remotemysql.com',
+user:'EQWzIIIodt',
+password:'mJcUb6PfVi',
+database:'EQWzIIIodt'
 });
  module.exports=connection;
